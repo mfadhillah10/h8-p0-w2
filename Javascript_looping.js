ca7dd302c1d81cf907c5ff59c33fed90b7130d90
mfadhillah10 <fadhilakbarr1@gmail.com>
2018-02-28 14:00:40 +0700

    Week 2 Exercise 6

---
+++ Javascript_looping.js
@@ -0,0 +1,18 @@
+// Membuat program looping
+
+var i = 2;
+var j = 20;
+
+//Looping pertama
+console.log('LOOPING PERTAMA')
+while(i <= 20){
+    console.log(i + ' - I love coding')
+    i += 2;
+}
+
+//Looping kedua
+console.log('LOOPING KEDUA')
+while(j >= 1){
+    console.log(j + ' - I will become a fullstack developer')
+    j -= 2;
+}
